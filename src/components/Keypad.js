c92f3153551cd54eb70358f9e44a751834be7d8b
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-01-06 05:14:51 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+export default class Keypad extends React.Component {
+
+    handleKeyUp = () => {
+        console.log('Entering password...')
+    }
+
+    render() {
+        return (
+            <div>
+                <input onKeyup={this.handleKeyUp} type="text" name="" id="" />
+            </div>
+        )
+    }
+}
